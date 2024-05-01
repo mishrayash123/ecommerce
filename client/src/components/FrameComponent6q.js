2764a7d2e50c3779da70bbaa7bc04172e8a1e9e2
Yash12345@@ <yashwantkannojiya8@gmail.com>
2024-05-01 15:50:01 +0530

    hello

--- client/src/components/FrameComponent6q.js
+++ client/src/components/FrameComponent6q.js
@@ -1,67 +1,117 @@
 const FrameComponent6 = () => {
   return (
     <div className="absolute top-[227px] left-[596px] bg-white box-border w-[962px] h-[699px] overflow-hidden text-left text-base text-dimgray-500 font-made-tommy border-[1px] border-solid border-silver-300">
-      <div className="absolute top-[61px] left-[45px] rounded-lg bg-whitesmoke-200 box-border w-[355px] h-10 overflow-hidden text-sm font-inter border-[1px] border-solid border-silver-300">
-        <div className="absolute top-[calc(50%_-_7px)] left-[17px]">
-          minishamanoj@gmail.com
-        </div>
+      
+      {/* Email ID */}
+      <div className="absolute top-[61px] left-[45px] rounded-lg bg-whitesmoke-200 box-border w-[355px] h-[40px] overflow-hidden text-sm font-inter border-[1px] border-solid border-silver-300">
+        <label htmlFor="email" className="block text-lg text-darkgray-500"></label>
+        <input
+          type="text"
+          id="email"
+          className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+          placeholder="minishamanoj@gmail.com"
+          style={{ fontSize: '14px' }}
+        />
       </div>
-      <div className="absolute top-[252px] left-[45px] rounded-lg bg-white box-border w-[355px] h-10 overflow-hidden font-inter border-[1px] border-solid border-silver-300">
-        <div className="absolute top-[calc(50%_-_9px)] left-[17px]">
-          Minisha
-        </div>
-      </div>
-      <div className="absolute top-[252px] left-[562px] rounded-lg bg-white box-border w-[355px] h-10 overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
-        <div className="absolute top-[calc(50%_-_9px)] left-[17px]">
-          Please Enter Your Birthday
-        </div>
-      </div>
-      <div className="absolute top-[352px] left-[562px] rounded-lg bg-white box-border w-[355px] h-10 overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
-        <div className="absolute top-[calc(50%_-_9px)] left-[17px]">
-          Mobile Number
-        </div>
-      </div>
-      <img
-        className="absolute top-[452px] left-[562px] rounded-lg w-[355px] h-[90px] overflow-hidden"
-        alt=""
-        src="/frame-186q.svg"
-      />
-      <div className="absolute top-[352px] left-[45px] rounded-lg bg-white box-border w-[355px] h-10 overflow-hidden font-inter border-[1px] border-solid border-silver-300">
-        <div className="absolute top-[calc(50%_-_9px)] left-[17px]">Mohan</div>
-      </div>
-      <div className="absolute top-[calc(50%_+_109.5px)] left-[75px] font-inter">
-        Men
-      </div>
-      <div className="absolute top-[calc(50%_+_109.5px)] left-[203px] font-inter">
-        Women
-      </div>
-      <div className="absolute top-[calc(50%_+_109.5px)] left-[355px] font-inter">
-        Other
-      </div>
-      <div className="absolute top-[19px] left-[45px] text-xl font-medium text-darkgray-800">
-        Email ID
-      </div>
+      
+      {/* General Information */}
       <div className="absolute top-[163px] left-[45px] text-xl text-dimgray-100">
         General Information
       </div>
-      <div className="absolute top-[217px] left-[45px] text-lg text-darkgray-500">
-        First Name *
+
+      {/* Left side inputs */}
+      <div className="left-side">
+        {/* First Name */}
+        <div className="absolute top-[217px] left-[45px] text-lg text-darkgray-500">
+          First Name *
+        </div>
+        <div className="absolute top-[252px] left-[45px] rounded-lg bg-white box-border w-[355px] h-[40px] overflow-hidden font-inter border-[1px] border-solid border-silver-300">
+          <input
+            type="text"
+            className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+            placeholder="Minisha"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        
+        {/* Last Name */}
+        <div className="absolute top-[315px] left-[45px] text-lg text-darkgray-500">
+          Last Name *
+        </div>
+        <div className="absolute top-[350px] left-[45px] rounded-lg bg-white box-border w-[355px] h-[40px] overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
+          <input
+            type="text"
+            className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+            placeholder="Last Name"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        
+        {/* Gender */}
+        <div className="absolute top-[420px] left-[45px] text-lg text-darkgray-500">
+          Gender *
+        </div>
+        <div className="absolute top-[450px] left-[45px]">
+          <label htmlFor="male" className="inline-block mr-2">
+            <input type="radio" id="male" name="gender" value="male" />
+            <span className="text-darkgray-700">Male</span>
+          </label>
+          <label htmlFor="female" className="inline-block mr-2">
+            <input type="radio" id="female" name="gender" value="female" />
+            <span className="text-darkgray-700">Female</span>
+          </label>
+          <label htmlFor="other" className="inline-block">
+            <input type="radio" id="other" name="gender" value="other" />
+            <span className="text-darkgray-700">Other</span>
+          </label>
+        </div>
       </div>
-      <div className="absolute top-[217px] left-[562px] text-lg text-darkgray-500">
-        Date of Birth
+
+      {/* Right side inputs */}
+      <div className="right-side">
+        {/* Date of Birth */}
+        <div className="absolute top-[217px] left-[562px] text-lg text-darkgray-500">
+          Date of Birth
+        </div>
+        <div className="absolute top-[252px] left-[562px] rounded-lg bg-white box-border w-[350px] h-[40px] overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
+          <input
+            type="date"
+            className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+            placeholder="Please Enter Your Birthday"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        
+        {/* Mobile Number */}
+        <div className="absolute top-[317px] left-[562px] text-lg text-darkgray-500">
+          Mobile Number *
+        </div>
+        <div className="absolute top-[352px] left-[562px] rounded-lg bg-white box-border w-[355px] h-[40px] overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
+          <input
+            type="text"
+            className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+            placeholder="Mobile Number"
+            style={{ fontSize: '14px' }}
+          />
+        </div>
+        
+        {/* Default Address */}
+<div className="absolute top-[420px] left-[562px] text-lg text-darkgray-500">
+  Default Address
+</div>
+<div className="absolute top-[450px] left-[562px] rounded-lg bg-white box-border w-[350px] h-[120px] overflow-hidden text-darkgray-200 font-inter border-[1px] border-solid border-silver-300">
+  <input
+    type="text"
+    rows="4" // This attribute may not be fully supported by all browsers for <input> tags
+    className="w-full h-full bg-transparent outline-none text-sm font-inter pl-2"
+    placeholder="Default Address"
+    style={{ fontSize: '14px' }}
+  />
+
+        </div>
       </div>
-      <div className="absolute top-[317px] left-[562px] text-lg text-darkgray-500">
-        Mobile Number *
-      </div>
-      <div className="absolute top-[417px] left-[562px] text-lg text-darkgray-500">
-        Default Address
-      </div>
-      <div className="absolute top-[317px] left-[45px] text-lg text-darkgray-500">
-        Last Name
-      </div>
-      <div className="absolute top-[417px] left-[45px] text-lg text-darkgray-500">
-        Last Name
-      </div>
+
+      {/* Images */}
       <img
         className="absolute top-[137.5px] left-[1.5px] max-h-full w-[960.5px]"
         alt=""
@@ -82,13 +132,10 @@ const FrameComponent6 = () => {
         alt=""
         src="/vector-62q.svg"
       />
-      <div className="absolute top-[459px] left-[173px] rounded-2xs box-border w-5 h-5 overflow-hidden border-[2px] border-solid border-silver-200" />
-      <div className="absolute top-[459px] left-[325px] rounded-2xs box-border w-5 h-5 overflow-hidden border-[2px] border-solid border-silver-200" />
-      <div className="absolute top-[459px] left-[45px] rounded-2xs box-border w-5 h-5 overflow-hidden border-[2px] border-solid border-salmon">
-        <div className="absolute top-[calc(50%_-_6px)] left-[calc(50%_-_6px)] rounded-2xs bg-salmon w-3 h-3" />
-      </div>
+
+      {/* Save Button */}
       <div className="absolute top-[626px] left-[calc(50%_-_75px)] rounded-md bg-salmon flex flex-row items-center justify-center py-[7px] px-11 text-5xl text-white">
-        <b className="relative">SAVE</b>
+        <button className="relative">SAVE</button>
       </div>
     </div>
   );
