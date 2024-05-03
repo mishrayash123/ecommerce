a66caa8c3be1e13dad3100aa300d1955fdae5c53
Yash12345@@ <yashwantkannojiya8@gmail.com>
2024-05-03 20:27:01 +0530

    hello

--- client/src/pages/Dashboard1.js
+++ client/src/pages/Dashboard1.js
@@ -25,36 +25,36 @@ const Dashboard1 = () => {
       </div>
       <div className="absolute top-[253px] left-[544px] rounded-4xl bg-white box-border w-[1099px] h-[1826px] overflow-hidden text-xl text-black border-[1px] border-solid border-lightgray">
         <div className="absolute top-[42px] left-[50px] flex items-center">
-          <label htmlFor="productTitle" className="text-black text-xl font-bold mr-8">Product Tittle</label>
-          <input type="text" id="productTitle" className="border border-solid border-darkgray-300 px-3 py-2 rounded w-[300px] text-xl" placeholder="Product Tittle" />
+          <label htmlFor="productTitle" className="text-black text-xl font-bold mr-8">Product Title</label>
+          <input type="text" id="productTitle" className="border border-solid border-darkgray-300 px-3 py-2 rounded w-[300px] text-xl" placeholder="Product Title" />
         </div>
         <div className="absolute top-[110px] left-[50px]">
-          <label htmlFor="productCategory" className="text-black text-xl font-bold mr-20">Product</label>
+          <label htmlFor="productCategory" className="text-black text-xl font-bold mr-20">Product Category</label>
           <input type="text" id="productCategory" className="border border-solid border-darkgray-300 px-3 py-2 rounded w-[300px] text-xl" placeholder="Enter Product Category" />
         </div>
         <div className="absolute top-[180px] left-[50px] flex items-center">
-          <label htmlFor="productImage" className="text-black text-xl font-bold mr-14">Product Image 1</label>
-          <input type="file" id="productImage" className="hidden" />
-          <label htmlFor="productImage" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
-          <span className="ml-4 text-l text-gray-500" id="noFileChosen" >No file chosen</span>
+          <label htmlFor="productImage1" className="text-black text-xl font-bold mr-14">Product Image 1</label>
+          <input type="file" id="productImage1" className="hidden" />
+          <label htmlFor="productImage1" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
+          <span className="ml-4 text-l text-gray-500" id="noFileChosen1" >No file chosen</span>
         </div>
         <div className="absolute top-[230px] left-[50px] flex items-center">
-          <label htmlFor="productImage" className="text-black text-xl font-bold mr-14">Product Image 2</label>
-          <input type="file" id="productImage" className="hidden" />
-          <label htmlFor="productImage" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
-          <span className="ml-4 text-l text-gray-500" id="noFileChosen" >No file chosen</span>
+          <label htmlFor="productImage2" className="text-black text-xl font-bold mr-14">Product Image 2</label>
+          <input type="file" id="productImage2" className="hidden" />
+          <label htmlFor="productImage2" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
+          <span className="ml-4 text-l text-gray-500" id="noFileChosen2" >No file chosen</span>
         </div>
         <div className="absolute top-[280px] left-[50px] flex items-center">
-          <label htmlFor="productImage" className="text-black text-xl font-bold mr-14">Product Image 3</label>
-          <input type="file" id="productImage" className="hidden" />
-          <label htmlFor="productImage" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
-          <span className="ml-4 text-l text-gray-500" id="noFileChosen" >No file chosen</span>
+          <label htmlFor="productImage3" className="text-black text-xl font-bold mr-14">Product Image 3</label>
+          <input type="file" id="productImage3" className="hidden" />
+          <label htmlFor="productImage3" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
+          <span className="ml-4 text-l text-gray-500" id="noFileChosen3" >No file chosen</span>
         </div>
         <div className="absolute top-[330px] left-[50px] flex items-center">
-          <label htmlFor="productImage" className="text-black text-xl font-bold mr-14">Product Image 4</label>
-          <input type="file" id="productImage" className="hidden" />
-          <label htmlFor="productImage" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
-          <span className="ml-4 text-l text-gray-500" id="noFileChosen" >No file chosen</span>
+          <label htmlFor="productImage4" className="text-black text-xl font-bold mr-14">Product Image 4</label>
+          <input type="file" id="productImage4" className="hidden" />
+          <label htmlFor="productImage4" className="border border-solid border-darkgray-300 px-3 py-2 rounded cursor-pointer" style={{ width: '120px', height: '14px', color: 'grey' }}>Choose File</label>
+          <span className="ml-4 text-l text-gray-500" id="noFileChosen4" >No file chosen</span>
         </div>
         <div className="absolute top-[420px] left-[50px]">
           <label htmlFor="productPrice" className="text-black text-xl font-bold mr-8">Product Price</label>
@@ -62,52 +62,30 @@ const Dashboard1 = () => {
         </div>
         <div className="absolute top-[500px] left-[50px]">
           <label htmlFor="availableSize" className="text-black text-xl font-bold mr-14">Choose Available Size</label>
-          <div className="flex flex-wrap">
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">XXS</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">XS</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">S</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">M</span>
-            </div>
-          </div>
-          <div className="flex flex-wrap">
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">L</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">XL</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">XXL</span>
-            </div>
-            <div className="w-10 h-10 bg-white border border-solid border-red-500 flex items-center justify-center mr-2 mb-2 rounded cursor-pointer hover:border-black">
-              <span className="text-red-500 font-medium text-sm">XXXL</span>
-            </div>
-          </div>
+          <select id="availableSize" className="border border-solid border-darkgray-300 px-3 py-2 rounded text-xl">
+            <option value="XXS">XXS</option>
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+            <option value="XXXL">XXXL</option>
+          </select>
         </div>
         <div className="absolute top-[700px] left-[50px]">
-  <label className="text-black text-xl font-bold mr-14 mb-8">Product Description</label>
-  <DescriptionEditor />
-</div>
- <div className="absolute top-[1200px] left-[50px]">
+          <label className="text-black text-xl font-bold mr-14 mb-8">Product Description</label>
+          <DescriptionEditor />
+        </div>
+        <div className="absolute top-[1200px] left-[50px]">
           <label htmlFor="productDetails" className="text-black text-xl font-bold mr-14 mb-8">Product Details</label>
           <ProductDetailsEditor />
         </div>
-
         <div className="absolute bottom-[50px] left-[500px]">
           <button onClick={handleInsertProduct} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-16 rounded">
             Insert Product
           </button>
-          </div>
-
-        
-      
+        </div>
       </div>
     </div>
   );
