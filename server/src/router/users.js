83c81de2e5f91a054471dc6a4e45563e5b176838
yash kumar mishra <mishrayash3778@gmail.com>
2024-05-11 20:14:09 +0530

    kjjk

--- server/src/router/users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { getAllUsers, deleteUser, updateUser } from '../controllers/users.js';
-
-export default (router) => {
-  router.post('/users', getAllUsers);
-  router.delete('/deleteuser/:id', deleteUser);
-  router.patch('/updateusers/:id',  updateUser);
-};
