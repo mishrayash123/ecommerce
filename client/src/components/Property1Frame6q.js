ecec98de70cb24b5434aae55fd61b0f5e0c08a41
Anurag Sidhu <anuragsidhu72@gmail.com>
2024-05-20 08:50:31 +0530

    FAQ  Responsive Done

--- client/src/components/Property1Frame6q.js
+++ client/src/components/Property1Frame6q.js
@@ -9,71 +9,78 @@ const Property1Frame6 = ({ frameDivTop, frameDivLeft }) => {
   }, [frameDivTop, frameDivLeft]);
 
   return (
+    <>
+  <div className="flex flex-col w-2/4 ">
+
+    <p className="ml-4">Tracking Order</p>
     <div
-      className="absolute top-[20px] left-[20px] flex flex-col items-start justify-start text-left text-xl text-black font-made-tommy"
-      style={property1Frame187Style}
-    >
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+      className="  flex flex-col w-full   p-4   text-left text-xl text-black  font-made-tommy "
+      >
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
-      <div className="w-[955px] bg-white box-border h-[69px] overflow-hidden shrink-0 flex flex-row items-center justify-start py-[22px] px-10 relative gap-[611px] border-[1px] border-solid border-silver-300">
-        <div className="w-[249px] absolute !m-[0] top-[22px] left-[40px] inline-block z-[0]">
+      <div className=" md:w-full bg-white box-border w-56 h-10 p-2   flex flex-row items-center justify-between    border-[1px] border-solid border-silver-300">
+        <div className="  md:text-lg text-sm ">
           Lorem ipsum dolor sit amet
         </div>
         <img
-          className="w-[15px] absolute !m-[0] top-[31px] left-[900px] h-[7px] z-[1]"
+          className="w-[15px]  h-[7px] z-[1]"
           alt=""
           src="/vector-68q.svg"
-        />
+          />
       </div>
     </div>
+          
+          </div>
+          </>
   );
 };
 
